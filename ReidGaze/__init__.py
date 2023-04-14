aca0fafaf2f83287a1ee96f12d779574d23297a8
XinluHe1 <130602087+XinluHe1@users.noreply.github.com>
2023-04-14 17:01:13 -0400

    Add files via upload

---
+++ ReidGaze/__init__.py
@@ -0,0 +1,32 @@
+from .reidgaze import processRequest
+
+
+def get_reidgaze(params):
+    frame_json,img_list = processRequest(params)
+    return frame_json,img_list
+
+'''
+example of params:
+params = {
+    "registrations":[
+        {'id': "214307", 
+        'registrationVideoPath':"/scratch/shared/whitehill/recording_id_6425d1ae29b832001e054b16/student_enrollment_214307.webm",
+        'length':28},
+        {'id': "222826", 
+        'registrationVideoPath':"/scratch/shared/whitehill/recording_id_6425d1ae29b832001e054b16/student_enrollment_222826.webm",
+        'length':12},
+        {'id': "236416", 
+        'registrationVideoPath':"/scratch/shared/whitehill/recording_id_6425d1ae29b832001e054b16/student_enrollment_236416.webm",
+        'length':13},
+        {'id': "253270", 
+        'registrationVideoPath':"/scratch/shared/whitehill/recording_id_6425d1ae29b832001e054b16/student_enrollment_253270.webm",
+        'length':12},
+    ],
+    "ReferDuration":4,
+    "videoPath":"/scratch/shared/whitehill/recording_id_6425d1ae29b832001e054b16/Terri-Reh_2023-03-30_PComputer-Science-P5-SP-2023---Terri-Reh---Flagstaff_Cx_Lx_video-device-unknown_12.15.09.285_Chunk0001.webm",
+    "startTime":0, 
+    "endTime":5,
+    "TestDuration":1
+}
+
+'''
